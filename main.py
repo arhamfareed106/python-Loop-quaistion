d10f17db9b41f394c7a40577a41a82a7b274fec6
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-09 21:57:43 +0500

    commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+            ###### Questions  1 ######
 # for i in  range(1,6):
 #     print(i, end=" ")
 
 # output 1 2 3 4 5 
+
+
+            ###### Questions  2 ######
 
 
 # for i in range(1,6):
@@ -9,6 +13,7 @@
 
 #  1 4 9 16 25
 
+            ###### Questions  3 ######
 
 
 # for i in range(1,11):
@@ -17,15 +22,26 @@
 
 # output 2 4 6 8 10
 
+            ###### Questions  4 ######
+
+
 
 # total=0
 # for i in range(1,110):
 #     total+=i
 # print(f"Sum is {total}")
 
+                    ###### Questions  4 ######
+
+
+
 # word = 'python'
 # for i in range(len(word)-1, -1, -1) :
 #     print(word[i], end=" ")
+
+
+                    ###### Questions  5 ######
+
 
 
 # vowels= "aeiou"
