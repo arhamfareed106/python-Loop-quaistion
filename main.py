e5e16146f9ad3d6df3f48e6920ecc97dd8ab52fd
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-09 16:23:07 +0500

    q1

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-for i in  range(1,6):
-    print(i, end=" ")
+# for i in  range(1,6):
+#     print(i, end=" ")
 
 # output 1 2 3 4 5 
 
 
+# for i in range(1,6):
+#     print(i ** 2 , end=" ")
+
+#  1 4 9 16 25
 
 
+
+# for i in range(1,11):
+#     if i % 2 ==0:
+#         print(i, end=" ")
+
+# output 2 4 6 8 10
 
 
